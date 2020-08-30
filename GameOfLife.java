e94763e33188a0a82d6f17eaa88c6c516da16fd2
RatonBaveur <57619823+RatonBaveur@users.noreply.github.com>
2020-08-30 23:53:43 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,19 +1,15 @@
 package GameOfLife;
 import java.awt.*;
-import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.util.*;
 import javax.swing.*;
 import javax.swing.Timer;
 
-public class GameOfLife {
-	
+import java.util.ArrayList;
+import java.util.List;
 
+public class GameOfLife {
     public static void main(String[] a) {
-    	
-    	
     	EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -22,65 +18,94 @@ public class GameOfLife {
                 } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
                     ex.printStackTrace();
                 }
-                JFrame frame = new JFrame("GameOfLife");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                JFrame window = new JFrame("GameOfLife");
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         		ImageIcon img = new ImageIcon("E:\\Dev\\MonWorkspace\\CPS tester\\src\\icon_GameOfLife.png");
-        		frame.setIconImage(img.getImage());
-                frame.add(background());
-                frame.setSize(500, 530);
-                frame.setVisible(true);
-                frame.setResizable(true); 
-                
-                
-                
-            }
-           ;
+        		window.setIconImage(img.getImage());
+        		window.add(background());
+        		window.setSize(600, 660);
+        		window.setVisible(true);
+        		window.setResizable(true);
+            };
         });
-  }
+}
+
 public static int raws=40;
 public static int columns=40;
+static JCheckBox[]BornConds= {new JCheckBox("0",false), new JCheckBox("1",false), new JCheckBox("2",false), new JCheckBox("3",true), new JCheckBox("4",false), new JCheckBox("5",false), new JCheckBox("6",false), new JCheckBox("7",false), new JCheckBox("8",false)};
+static JCheckBox[]SurviveConds= {new JCheckBox("0",false), new JCheckBox("1",false), new JCheckBox("2",true), new JCheckBox("3",true), new JCheckBox("4",false), new JCheckBox("5",false), new JCheckBox("6",false), new JCheckBox("7",false), new JCheckBox("8",false)};
 
- public static JPanel createGrid() {
-	 	previousgen previousgen = new previousgen();
-	    JPanel grid = new JPanel();
-	    GridLayout layout = new GridLayout(raws,columns);
-	    layout.setHgap(1);
-	    layout.setVgap(1);
-	    grid.setLayout(layout);
-	    for (int x=0;x<raws;x++) {
-	    	for(int y=0;y<columns;y++) {
-	    		grid.add(previousgen.grid[x][y]);
-	    	}
-	    }
-	    return grid;
-	    
-	  
-  }
- public static boolean getRandomBoolean() {
+public static boolean getRandomBoolean() {
      return Math.random() < 0.5;
  }
+
 public static JPanel background() {
-	Timer t = new Timer(0, null);
+	Timer t = new Timer(0, null);//creates the timer for repeatedly spawning the next generations
 	t.setRepeats(true);
-	t.setDelay(250);//setDelay(ms)
+	t.setDelay(125);//setDelay(Ms) sets repeat delay of start button
 	JPanel background = new JPanel();
 	JPanel buttons = new JPanel();
+	JPanel grid = new JPanel();
 	FlowLayout flawlayout = new FlowLayout();
 	BorderLayout borderlayout = new BorderLayout();
+	GridLayout gridlayout = new GridLayout(raws,columns);
 	buttons.setLayout(flawlayout);
 	background.setLayout(borderlayout);
-	background.add(createGrid(),BorderLayout.CENTER);
+ 	previousgen previousgen = new previousgen();
+    gridlayout.setHgap(1);
+    gridlayout.setVgap(1);
+    grid.setLayout(gridlayout);
+    for (int x=0;x<raws;x++) {
+    	for(int y=0;y<columns;y++) {
+    		grid.add(previousgen.grid[x][y]);
+    	}
+    }
 	JButton start = new JButton("Start");
 	JButton randomgen = new JButton("Random Generation");
 	JButton clear = new JButton("Clear");
 	JButton nextgen = new JButton("Next Generation");
 	JButton stop = new JButton("Stop");
+	JButton settings = new JButton("Delay");
+	JButton rules = new JButton("rules");
+	String[] delaylist = {"Delay","125","250","500","1000"};
 	background.add(buttons, BorderLayout.NORTH);
+	background.add(grid, BorderLayout.CENTER);
 	buttons.add(start);
 	buttons.add(randomgen);
 	buttons.add(nextgen);
 	buttons.add(clear);
 	buttons.add(stop);
+	buttons.add(rules);
+	buttons.add(settings);
+	
+	rules.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == rules) {
+				JDialog RulesWindow = new JDialog();
+				RulesWindow.setTitle("Rules");
+				RulesWindow.setDefaultCloseOperation(RulesWindow.DISPOSE_ON_CLOSE);
+				JPanel RulesPanel = new JPanel();
+				GridLayout RulesLayout = new GridLayout(2,10);
+				JLabel bornlabel = new JLabel("cell borns");
+				JLabel survivelabel = new JLabel("cell survives");
+				RulesLayout.setHgap(5);
+				RulesLayout.setVgap(10);
+				RulesPanel.setLayout(RulesLayout);
+				RulesPanel.add(bornlabel);
+				for (int i=0;i<8;i++) {
+					RulesPanel.add(BornConds[i]);
+				};
+				RulesPanel.add(survivelabel);
+				for (int i=0;i<8;i++) {
+					RulesPanel.add(SurviveConds[i]);
+				};
+				RulesWindow.add(RulesPanel);
+				RulesWindow.setSize(450,200);
+				RulesWindow.setVisible(true);
+				System.out.println("rules");
+			}
+		}
+	});
 	t.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			nextgeneration();
@@ -135,9 +160,32 @@ public static JPanel background() {
 			}
 		}
 	});
+	settings.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == settings) {
+				JOptionPane WindowSetting = new JOptionPane();
+				String GenerationDelay = (String) WindowSetting.showInputDialog(null,"Select a delay between each generation","Delay",JOptionPane.QUESTION_MESSAGE, null, delaylist,delaylist[0]);
+				if ("125".equals(GenerationDelay)) {
+					System.out.println("125");
+					t.setDelay(125);
+				} else if ("250".equals(GenerationDelay)) {
+					System.out.println("250");
+					t.setDelay(250);
+				}else if ("500".equals(GenerationDelay)) {
+					System.out.println("500");
+					t.setDelay(500);
+				}else if ("1000".equals(GenerationDelay)) {
+					System.out.println("1000");
+					t.setDelay(1000);
+				};
+			};
+		};
+	});
 	return background;
 	
+	
 }
+
 public static void nextgeneration() {
 		previousgen previousgen = new previousgen();
 		boolean[][] nextgen= {
@@ -198,12 +246,12 @@ public static void nextgeneration() {
 							}}catch(Exception e1) {};
 						}
 					}
-					if (voisins==3&!previousgen.grid[y][x].isLife()) {
-						nextgen[y][x]=true;
-					} else if ((voisins==3|voisins==2)&previousgen.grid[y][x].isLife()) {
-						nextgen[y][x]=true;
-					} else {
-						nextgen[y][x]=false;
+					for (int i=0;i<8;i++) {
+						if(BornConds[i].isSelected()&i==voisins&!previousgen.grid[y][x].isLife()) {
+							nextgen[y][x]=true;
+						} else if (SurviveConds[i].isSelected()&i==voisins&previousgen.grid[y][x].isLife()) {
+							nextgen[y][x]=true;
+						}
 					}
 			}
 		}
